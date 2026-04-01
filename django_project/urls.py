f114845d5bb5136dbb9d22a7a50f1d8208c09020
DominicXyd <dxydakis@unomaha.edu>
2026-03-31 20:24:39 -0500

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,19 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.home, name="home"),
-    path("search/", views.search, name="search"),
-    path("tickets-page/", views.view_tickets_page, name="tickets_page"),
-
-    path("tickets/", views.ticket_list, name="ticket_list"),
-    path("my-tickets/", views.my_tickets, name="my_tickets"),
-    path("tickets/status/<str:status_name>/", views.tickets_by_status, name="tickets_by_status"),
-    path("tickets/<int:ticket_id>/", views.ticket_detail, name="ticket_detail"),
-    path("tickets/create/", views.create_ticket, name="create_ticket"),
-    path("tickets/<int:ticket_id>/comment/", views.add_comment, name="add_comment"),
-]
+    path("admin/", admin.site.urls),
+    path("", include("easy_ticket_app.urls")),
 ]
